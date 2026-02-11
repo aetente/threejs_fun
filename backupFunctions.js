51c60e78ec8169e734dd32d51888378175daecdd
Yehoshua Kopirin <aetente@gmail.com>
2026-02-11 21:00:40 +0200

    backlogged latest pattern function

--- backupFunctions.js
+++ backupFunctions.js
@@ -549,6 +549,68 @@ function pattern1_1(scene, pointsArray, options) {
   }
 }
 
+function pattern1_2(scene, pointsArray, options) {
+  let limit = options?.limit || 50;
+  let maxLines = options?.maxLines || 100
+  const scale = options?.scale || 1;
+  const offset = options?.offset || new THREE.Vector3(0, 0, 0.1);
+  const initPoint = options?.initPoint || getRandomPointBetweenPoints(pointsArray);
+  const refPoint = options?.refPoint || new Vector3(0,0,0)
+  const initAngle = options?.initAngle || 0
+  let scaleSize = 0.05 * scale
+  let points = [];
+  let angleVal = 0;
+  let previousAngle = initAngle;
+  
+  const lineWidth = 5;
+  
+  for (let i = 0; i < limit; i++) {
+    const iScale = i/limit
+    let nextPos = getRandomPointBetweenPoints(pointsArray);
+    let previousPos = getRandomPointBetweenPoints(pointsArray);
+    const idByPos = previousPos.x + previousPos.y + previousPos.z
+    previousAngle = initAngle
+    for (let j = 0; j < maxLines; j++) {
+      // scaleSize = (psin(sin(i*2)/10) * 0.1 + 0.05) * scale
+      const jScale = sin(j/maxLines * PI)
+      const distToRef = previousPos.distanceTo(refPoint)+1
+      const angleCap = PI/3.5
+      //const angleChange = sin( sin(20*i + idByPos) * angleCap) * angleCap
+      const indexId = sin((i+j)/10)
+      const angleChange = jScale* sin(
+        sin(5*sin(indexId*PI)/distToRef + idByPos)
+        * angleCap
+      ) * angleCap
+      //const angleChange = sin(i + idByPos) * angleCap
+      //const angleChange = 0
+      angleVal = previousAngle + angleChange
+      scaleSize = psin(angleVal / PI / 3) * 0.1 * scale /1.4;
+      previousAngle = angleVal;
+      const currentPos = new THREE.Vector3(
+        sin(angleVal) * scaleSize,
+        cos(angleVal) * scaleSize,
+        0
+      );
+      nextPos = previousPos.clone().add(currentPos);
+        
+      const sunsetPalleteIndex = psin(angleVal / angleCap * PI * 2) * (testPalette1.length - 1);
+      const color = testPalette1[Math.floor(sunsetPalleteIndex)];
+      const previousPosWithOffset = previousPos.clone().add(offset);
+      const nextPosWithOffset = nextPos.clone().add(offset);
+      drawLine(scene, [previousPosWithOffset, nextPosWithOffset], { lineWidth: i == 0 ? 5 : lineWidth, color: i === 0 ? 0xffffff : color });
+      
+      previousPos.copy(nextPos);
+      points.push(nextPos.clone());
+    }
+    points = []
+    const circle = new THREE.CircleGeometry(0.002 * random() + 0.02, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const shape = new THREE.Mesh(circle, material);
+      shape.position.set(nextPos.x, nextPos.y, nextPos.z + 0.2);
+    //scene.add(shape);
+  }
+}
+
 function coat1(pointsArray, options) {
   const shapesArray = []
   const scale = options?.scale || 1
